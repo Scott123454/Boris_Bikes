ef56d8499e9ee9d574917b623c6edbb026de4bfa
scott123454 <scott123454@hotmail.co.uk>
2014-07-15 08:09:54 +0100

    additional tests for person class to test bike can be dockec and released from a station using doubles

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,14 +3,14 @@ require 'person'
 describe Person do
 
 	let(:person) {Person.new}
+	let(:person_with_bike) {Person.new(:bike)}
 
 	it 'has no bike when created' do
 		expect(person).not_to have_bike
 	end
 
 	it 'can have a bike' do
-		person = Person.new(:bike)
-		expect(person).to have_bike
+		expect(person_with_bike).to have_bike
 	end
 
 	it 'can have a bike accident' do
@@ -33,9 +33,14 @@ describe Person do
 
 	it 'returns a bike to a station' do
 		station = double :station
-		expect(station).to receive(:dock)
-		person.return_bike_to(station)
+		expect(station).to receive(:dock).with(:bike)
+		person_with_bike.return_bike_to(station)
+	end
 
+	it 'has no bike after docking' do
+		station = double :station, dock: nil
+		person_with_bike.return_bike_to(station)
+		expect(person_with_bike).not_to have_bike
 	end
 
 end
