8fcdf5415fbe80a16d3f4a0d9671965f8267dee2
scott123454 <scott123454@hotmail.co.uk>
2014-07-15 09:43:11 +0100

    additional tests written for docking station and refactoring work on docking station lib file

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,7 @@
 class DockingStation
 
+	BROKEN_BIKE_SELECTOR = ->(bike){bike.broken?}
+
 	def initialize(bikes = [])
 		@bikes = bikes
 	end	
@@ -9,16 +11,28 @@ class DockingStation
 	end
 
 	def release_bike
-		@bikes.delete(available_bikes.pop)
+		release(available_bikes.pop)
 	end
 
 	def available_bikes
-		@bikes.reject {|bike| bike.broken?}
+		@bikes.reject(&BROKEN_BIKE_SELECTOR)
+	end
+
+	def broken_bikes
+		@bikes.select(&BROKEN_BIKE_SELECTOR)
+	end
+
+	def release(bike)
+		@bikes.delete(bike)
 	end
 
 	def dock(bike)
 		@bikes << bike
 		nil
+	end
+
+	def release_broken_bikes
+		broken_bikes.map{|bike| release(bike)}
 	end
 
 end
